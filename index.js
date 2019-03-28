d694d24e63daf0697b1a30fb5d36106e34e1f4ef
EricMChavez <eric@emcaudio.com>
2019-03-27 21:15:33 -0500

    completed input

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 let display = '';
 let value1 = '';
-let value2 = '';
 let operation = '';
 function input(btn) {
 	display = display.concat(btn);
@@ -16,3 +15,30 @@ function allClear() {
 	operation = '';
 	refresh();
 }
+function operater(task) {
+	operation = task;
+	value1 = display;
+	display = '';
+	refresh();
+}
+function equate() {
+	if (operation == '+') {
+		addition();
+	} else if (operation == '-') {
+		subtraction();
+	} else if (operation == 'x') {
+		multiplication();
+	} else if (operation == '÷') {
+		division();
+	} else {
+		document.getElementById('display').innerHTML = 'Select Operation';
+		setTimeout(function() {
+			refresh();
+		}, 1000);
+	}
+}
+
+function addition() {}
+function subtraction() {}
+function multiplication() {}
+function division() {}
